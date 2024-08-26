8dfb46d18aa6ad67192617a8c15fe6a129d3af3c
Ramez <ramezyouakimf@gmail.com>
2024-08-26 17:46:19 +0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [userData, setUserData] = useState<null | any>(null)
+  const [data, setData] = useState<null | any>(null)
+
 
   useEffect(() => {
     getApiData()
@@ -17,7 +19,7 @@ export default function Home() {
   const getApiData = async () => {
     const res = await fetch("https://api.telegram.org/7529380285:AAHj4qaLHkTo6IWfYWHXqspZ29fn05Wq_BI/getMe")
     const resParsed = await res.json()
-    console.log(resParsed)
+    setData(resParsed)
   }
   return (
     <>
@@ -28,6 +30,8 @@ export default function Home() {
             <li>Name: {userData.first_name}  {userData.last_name}</li>
             <li>Is premium: {userData.is_premium}</li>
           </ul>
+
+          <p>{data}</p>
         </>
         :
         <h1>Loading ...</h1>}
